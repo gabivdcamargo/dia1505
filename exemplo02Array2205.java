5642018327450d5b28fe406f24f64b36494aa312
gabivdcamargo <159277563+gabivdcamargo@users.noreply.github.com>
2024-05-22 12:14:06 -0300

    Vetores

---
+++ exemplo02Array2205.java
@@ -0,0 +1,10 @@
+package vetores;
+
+public class exemplo02Array2205 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
--- javaVetor2205.java
+++ javaVetor2205.java
@@ -6,24 +6,21 @@ public class javaVetor2205 {
 
 	public static void main(String[] args) {
 		Scanner ler = new Scanner(System.in);
-		int val=0, soma=0;
+		int soma=0;
 		int a [] = new int [5];
 
 		for (int i=0; i<=4; i++)   {
-			System.out.println ("Informe um valor" + i +"..:");	}
-		int i;
-		a[i]= ler.nextInt ();
-		
-		soma = soma + a[i];
-		
-		if (soma>30)  {
-			System.out.println("O valor é maior que 30, a soma ´´é = ");
-		}
-	
-		
+			System.out.println ("Informe um valor");
+			a[i]= ler.nextInt ();
 
-		for (int  i1=0; i1<5; i1++) {
-			System.out.println (a[i]);
+			soma = soma + a[i];
+
+			if (soma>30)  {
+				System.out.println("O valor é maior que 30, a soma é = " + soma);
+			}
+			else {
+				System.out.println ("O valor é menor que 30, a soma é = " + soma);
+			}
 
 		}
 		ler.close();
